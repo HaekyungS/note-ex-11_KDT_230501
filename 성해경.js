f9637aae4484a855ef27033aa65a6363bb28a775
HaekyungSeong <haekyungs95@gmail.com>
2023-05-02 01:26:28 +0900

    forEach를 이용해 배열 재설정

--- 성해경.js
+++ 성해경.js
@@ -2,7 +2,7 @@
 // def flatten(data):
 //     output = []
 //     for item in data:
-//         if typeof(item) == list:
+//         if type(item) == list:
 //             output+=flatten(item)
 //         else:
 //             output.append(item)
@@ -16,17 +16,19 @@ const example = [[1, 2, 3], [4, [5, 6]], 7, [8, 9]];
 function flatten(data) {
   let output = []
   for (item in data) {
-    console.log(data[item])
+    // console.log(data[item])
     // 쓰고보니까 typeof 에 array가 어딨나. object 로 나오지않나
     if (Array.isArray(data[item])) {
-      console.log(flatten(data[item]))
-      // output.push(flatten(data[item]))
+      // console.log(flatten(data[item]))
+      flatten(data[item]).forEach((element)=>{
+        output.push(element);
+      })
     } else {
       // console.log(data[item])
       output.push(data[item])
     }
   }
-  console.log('나는 아웃풋' + output)
+  // console.log('나는 아웃풋' + output)
 
   return output
 }
