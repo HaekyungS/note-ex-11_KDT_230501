77609c8752b874fd7ded1f239890953cc4a1c4e5
HaekyungSeong <haekyungs95@gmail.com>
2023-05-02 00:43:07 +0900

    js 로 추측성 작성해보기 1탄

--- 성해경.js
+++ 성해경.js
@@ -8,3 +8,26 @@
 //             output.append(item)
 //     return output
 // print(flatten(example))
+
+// 우선 예상되는 뉘앙스 그대로 작성해보기
+
+const example=[[1,2,3],[4,[5,6]],7,[8,9]];
+
+function flatten(data){
+  let output=[]
+  for(item in data){
+    console.log('나는 아이템'+item)
+    // 쓰고보니까 typeof 에 array가 어딨나. object 로 나오지않나
+    if(Array.isArray(item)){
+      console.log('나는 재귀함수를 실행한'+flatten(item))
+      output +=flatten(item)
+      console.log('나는 아웃풋'+output)
+    }else{
+      console.log(item)
+      output.push(item)
+    }
+  }
+  return output
+}
+
+console.log(flatten(example))
