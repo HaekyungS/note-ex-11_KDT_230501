9af716fb5f83d834a666e21e1938e55e10cbe847
HaekyungSeong <haekyungs95@gmail.com>
2023-05-01 23:58:10 +0900

    기본틀 커밋

---
+++ 성해경.js
@@ -0,0 +1,10 @@
+// example = [[1,2,3],[4,[5,6],7,[8,9]]]
+// def flatten(data):
+//     output = []
+//     for item in data:
+//         if typeof(item) == list:
+//             output+=flatten(item)
+//         else:
+//             output.append(item)
+//     return output
+// print(flatten(example))
